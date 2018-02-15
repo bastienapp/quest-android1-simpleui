f6d86bfa007475525251888904bf776e60f68c63
bastienwcs <bastien@wildcodeschool.fr>
2018-02-15 18:07:26 +0100

    Conventions

--- app/src/main/java/fr/wildcodeschool/simpleui/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/simpleui/MainActivity.java
@@ -1,7 +1,7 @@
 package fr.wildcodeschool.simpleui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -16,33 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button accept = findViewById(R.id.accept);
-        final EditText firstname = findViewById(R.id.firstname);
-        final EditText lastname = findViewById(R.id.lastname);
-        final TextView success = findViewById(R.id.congrats);
+        final Button buttonAccept = findViewById(R.id.button_accept);
+        final EditText editFirstname = findViewById(R.id.edit_firstname);
+        final EditText editLastname = findViewById(R.id.edit_lastname);
+        final TextView textSuccess = findViewById(R.id.text_congrats);
 
         CheckBox checkboxWilder = findViewById(R.id.checkbox_wilder);
         checkboxWilder.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                firstname.setEnabled(checked);
-                lastname.setEnabled(checked);
-                accept.setEnabled(checked);
+                editFirstname.setEnabled(checked);
+                editLastname.setEnabled(checked);
+                buttonAccept.setEnabled(checked);
             }
         });
 
-        accept.setOnClickListener(new View.OnClickListener() {
+        buttonAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String firstnameContent = firstname.getText().toString();
-                String lastnameContent = lastname.getText().toString();
-                if (firstnameContent.isEmpty() || lastnameContent.isEmpty()) {
+                String firstname = editFirstname.getText().toString();
+                String lastname = editLastname.getText().toString();
+                if (firstname.isEmpty() || lastname.isEmpty()) {
                     Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_LONG).show();
-                    success.setVisibility(View.GONE);
+                    textSuccess.setVisibility(View.GONE);
                 } else {
                     String congrats = getResources().getString(R.string.congratulation);
-                    success.setText(String.format(congrats, firstnameContent, lastnameContent));
-                    success.setVisibility(View.VISIBLE);
+                    textSuccess.setText(String.format(congrats, firstname, lastname));
+                    textSuccess.setVisibility(View.VISIBLE);
                 }
             }
         });
